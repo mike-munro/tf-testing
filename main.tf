d64ca6917b5bebb9d6d86f13989e1aaaabab438d
Michael Munro <31053024+mike-munro@users.noreply.github.com>
2023-03-27 09:49:14 +0000

    windows

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
